d696a2dd34a351277b9d1461803bf42eba68f84a
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-04 18:41:46 -0400

    commit tree 3451f37dfea82e65303877094e0a5fbb323c5e48

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards( names, event ) {
+    let thanks = []
+    for ( let i = 0; i < names.length; i++ ) {
+      thanks.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return thanks
+}
+
+function countDown( start ) {
+
+    while ( start > 0 ) {
+      console.log( start );
+      start -= 1;
+    }
+    console.log( start );
+}
